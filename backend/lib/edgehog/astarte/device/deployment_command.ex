cf0de3423d01776dcb4641c3701d1f7f91e56bde
Francesco Noacco <francesco.noacco@secomind.com>
2024-10-17 10:49:03 +0200

    wip

--- backend/lib/edgehog/astarte/device/deployment_command.ex
+++ backend/lib/edgehog/astarte/device/deployment_command.ex
@@ -8,8 +8,8 @@ defmodule Edgehog.Astarte.Device.DeploymentCommand do
   @interface "io.edgehog.devicemanager.apps.DeploymentCommand"
 
   @impl Edgehog.Astarte.Device.DeploymentCommand.Behaviour
-  def send_deployment_command(client, device_id, release, command) do
-    path = "/#{release}/command"
+  def send_deployment_command(client, device_id, deployment_id, command) do
+    path = "/#{deployment_id}/command"
     AppEngine.Devices.send_datastream(client, device_id, @interface, path, command)
   end
 end
